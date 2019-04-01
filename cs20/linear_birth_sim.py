1b8dc79f5dbdf03471bca9583a123e901771757c
Inno Jia <jdgets111@gmail.com>
2019-04-01 22:39:13 +0800

    Add birth linear use tf.data.Dataset Mon Apr  1 22:39:12 CST 2019

--- cs20/linear_birth_sim.py
+++ cs20/linear_birth_sim.py
@@ -11,7 +11,14 @@ DATA_FILE = 'data/birth_life_2010.txt'
 epoches = 100
 
 data, n_samples = utils.read_birth_life_data(DATA_FILE)
-X, Y = tf.placeholder(name='X', dtype=tf.float32), tf.placeholder(name='Y', dtype=tf.float32)
+
+# Use tf.data.Dataset instead of placeholder
+dataset = tf.data.Dataset.from_tensor_slices((data[:,0], data[:,1]))
+iterator = dataset.make_initializable_iterator()
+
+X, Y = iterator.get_next()
+
+#X, Y = tf.placeholder(name='X', dtype=tf.float32), tf.placeholder(name='Y', dtype=tf.float32)
 
 w = tf.get_variable('weight', initializer=tf.constant(0.0))
 b = tf.get_variable('bias', initializer=tf.constant(0.0))
@@ -27,16 +34,22 @@ with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
     for i in range(epoches):
+        sess.run([iterator.initializer])
         total_loss = 0
-        for x,y in data:
-            _, l = sess.run([optimizer, loss], feed_dict={X:x, Y: y})
-            total_loss += l
+        #for x,y in data:
+        try:
+            _, l = sess.run([optimizer, loss])
+            total_loss += l 
+        except tf.errors.OutOfRangeError:
+            pass
         print(f'Epoch {i}: {total_loss/n_samples}')
                 
     writer.close()
     w_out, b_out = sess.run([w, b])
-print('Took {time.time() - star} seconds')
-plt.plot(data[:,0], data[:,1], 'bo', label='Real data')
-plt.plot(data[:,0], data[:,0] * w_out + b_out, 'r', label='Predicted data')
-plt.legend()
-plt.show()
+
+print(f'Took {time.time() - star} seconds')
+
+#plt.plot(data[:,0], data[:,1], 'bo', label='Real data')
+#plt.plot(data[:,0], data[:,0] * w_out + b_out, 'r', label='Predicted data')
+#plt.legend()
+#plt.show()
