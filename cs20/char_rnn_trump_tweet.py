ca339a3b3e5a69f848de1b6a0d9ef9ab4d9d0baa
Inno Jia <jdgets111@gmail.com>
2019-04-08 23:05:56 +0800

    Add char RNN model Mon Apr  8 23:05:56 CST 2019

--- cs20/char_rnn_trump_tweet.py
+++ cs20/char_rnn_trump_tweet.py
@@ -45,19 +45,28 @@ def read_data(filename, vocab, window, overlap):
                 chunk += [0] * (window - len(chunk))
                 yield chunk
 
+def read_batch(stream, batch_size):
+    batch = []
+    for ele in stream:
+        batch.append(ele)
+        if len(batch) == batch_size:
+            yield batch
+            batch = []
+    yield batch
+
 
 ## Define super arg of the model
 HIDDEN_SIZE = [128, 256]
 BATCH_SIZE = 64
 LR = 0.0003
-SKIP_STEP = 1
+SKIP_STEP = 500
 NUM_STEP = 50    #用于RNN展开
 LENGTH = 200     # length of the generated text
 
 
 
 class CharRNN:
-    def __init__(self, model, hidden_size, barch_size, skip_step, length, 
+    def __init__(self, model, hidden_size, batch_size, skip_step, length, 
             num_step, lr):
         self.model = model
         self.path = f'data/{model}.txt'
@@ -75,10 +84,108 @@ class CharRNN:
         self.skip_step = skip_step
         self.num_step = num_step
         self.length = length
+        self.temp = tf.constant(1.5)
         self.gstep = tf.get_variable('global', dtype=tf.int32, trainable=False, 
                 initializer=tf.constant(0))
 
     def create_rnn(self, seq):
-        layers = [keras.layers.GRUCell(size) for size in self.hidden_size]
-        
+        layers = [tf.nn.rnn_cell.GRUCell(size) for size in self.hidden_size]
+        cells = tf.nn.rnn_cell.MultiRNNCell(layers) 
+        batch = tf.shape(seq)[0]
+        zero_states = cells.zero_state(batch, dtype=tf.float32)
+        self.in_state = tuple([tf.placeholder_with_default(state,
+                [None, state.shape[1]]) for state in zero_states])
+
+        # All the seq were packed to the same length
+        # now get the real length of the seq
+        length = tf.reduce_sum(tf.reduce_max(tf.sign(seq), 2), 1)
+        self.output, self.out_state = tf.nn.dynamic_rnn(cells, seq, length, self.in_state)
+
+    def create_mode(self):
+        seq = tf.one_hot(self.seq, len(self.vocab))
+        self.create_rnn(seq)
+        self.logits = tf.layers.dense(self.output, len(self.vocab), None)
+        loss = tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits[:, :-1],
+                labels=seq[:, 1:])
+        self.loss = tf.reduce_sum(loss)
+
+        # sample the next character from MaxWell-Boltzman Distribution
+        # with temperature temp. It workd equally well without exp
+        self.sample = tf.multinomial(tf.exp(self.logits[:, -1] / self.temp), 1)[:, 0]
+        self.opt = tf.train.AdamOptimizer(self.lr).minimize(self.loss,
+                global_step=self.gstep)
+
+
+    def _create_summaries(self):
+        with tf.name_scope('summary'):
+            tf.summary.scalar('loss', self.loss)
+            tf.summary.histogram('histogram_loss', self.loss)
+            self.summary_ops = tf.summary.merge_all()
+
+
+    def train(self):
+        self._create_summaries()
+        saver = tf.train.Saver()
+        start = time.time()
+        min_loss = None
+
+        with tf.Session() as sess:
+            writer = tf.summary.FileWriter('./graphs/char_rnn', sess.graph)
+            sess.run(tf.global_variables_initializer())
+
+            # restore ckpt 
+            ckpt = tf.train.get_checkpoint_state(os.path.dirname(f'checkpoints/{self.model}/checkpoint'))
+            if ckpt and ckpt.model_checkpoint_path:
+                saver.restore(sess, ckpt.model_checkpoint_path)
+
+            step = self.gstep.eval()
+            stream = read_data(self.path, self.vocab, self.num_step, self.num_step//2)
+            data = read_batch(stream, self.batch_size)
+
+            while True:
+                batch = next(data)
+            
+                batch_loss, _, summary = sess.run([self.loss, self.opt, self.summary_ops], {self.seq: batch})
+                writer.add_summary(summary, global_step=step)
+
+                if (step + 1) % self.skip_step == 0:
+                    print(f'Step {step} loss:{batch_loss}, time:{time.time() - start}')
+                    self.online_infer(sess)
+                    start = time.time()
+                    ckpt_name = f'checkpoints/{self.model}/char_rnn'
+                    if min_loss is None:
+                        saver.save(sess, ckpt_name, step)
+                    elif batch_loss < min_loss:
+                        saver.save(sess, ckpt_name, step)
+                        min_loss = batch_loss
+                step += 1
+
+    def online_infer(self, sess):
+        for seed in ['Hillary', 'I', 'R', 'T', '@', 'N', 'M', '.', 'G', 'A', 'W']:
+            sentence = seed
+            state = None
+            for _ in range(self.length):
+                batch = [encoder(sentence[-1], self.vocab)]
+                feed = {self.seq: batch}
+                if state is not None: # for the first decoder step, the state is None
+                    for i in range(len(state)):
+                        feed.update({self.in_state[i]: state[i]})
+                index, state = sess.run([self.sample, self.out_state], feed)
+                sentence += decoder(index, self.vocab)
+            print('\t' + sentence)
+
+
+
+def main():
+    model = 'trump_tweets'
+    utils.safe_mkdir('checkpoints/' + model)
+
+    lm = CharRNN(model, HIDDEN_SIZE, BATCH_SIZE, SKIP_STEP, LENGTH, NUM_STEP, LR)
+    lm.create_mode()
+    lm.train()
+
+if __name__ == '__main__':
+    main()
+
+
 
